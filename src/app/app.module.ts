7c3b21599878ca604d1e96140759519bd5043dcc
Tatiana <tatiana@simplemoviles.com>
2018-06-25 10:31:23 -0400

    mejoras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FontAwesomeModule,
     HttpClientModule,
     SharedModule,
     AppRoutingModule,
