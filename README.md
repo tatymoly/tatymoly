ddc2c2640a7e801afdf97128ce02e9ee4875eb44
Tatiana Molina <tatymoly@Tatianas-MacBook-Air.local>
2018-12-12 10:50:29 -0500

    update

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
 
+Update to Angular 7
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
@@ -25,4 +27,5 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
 # tatymoly
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,15 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       {
-        path: "projects",
-        loadChildren: "./projects/projects.module#ProjectsModule"
+        path: '',
+        loadChildren: './about/about.module#AboutModule',
       },
-      {
-        path: "",
-        loadChildren: "./about/about.module#AboutModule"
-      }
-    ])
+    ]),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
